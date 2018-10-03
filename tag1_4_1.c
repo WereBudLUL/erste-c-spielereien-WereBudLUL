18889c38e7926461158f65eb434ad09001a03d2b
Gorbach <43406162+WereBudLUL@users.noreply.github.com>
2018-10-03 06:16:50 +0200

    Create tag1_4_1.c

---
+++ tag1_4_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int start=10;start>0;start--)
+    {
+        printf("%d\n",start);
+        
+        if(start==1)
+        {
+            printf("Takeoff!");
+        }
+    }
+}
